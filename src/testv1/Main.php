1cb8b6b429effa483bb807cdb14d6792950e3993
imwood04 <41460038+imwood04@users.noreply.github.com>
2018-09-25 00:53:07 -0400

    Started Main.php

--- src/testv1/Main.php
+++ src/testv1/Main.php
@@ -2,3 +2,20 @@
 
 namespace test;
 
+use pocketmine\server;
+use pocketmine\player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase{
+  
+  public function onEnable(){
+    $this->getLogger->Info("Testv1 Has Been Enabled!");
+    }
+  
+  public function onDisable(){
+    $this->getLogger->Info("Testv1 Has Been Disabled!");
+    }
+  
+  publick function onCommand(CommandSender $command, 
