be27528f66085b33edfc7a0bfbf42a347b7f282a
AlexPads <37447608+AlexPads@users.noreply.github.com>
2018-09-25 14:29:54 -0400

    awaiting aproval

---
+++ src/testv1/main2.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace testv1;
+
+use pocketmine\server;
+use pocketmine\player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\event\Listener;
+use pocketmine\item\Item;
+
+class Main extends PluginBase{
+  
+  public function onEnable(){
+    $this->getLogger->Info("Testv1 Has Been Enabled!");
+    }
+  
+  public function onDisable(){
+    $this->getLogger->Info("Testv1 Has Been Disabled!");
+    }
+  
+  public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args){
+    if ($sender instanceof Player);
+         switch($cmd->getName()){
+             case "test":
+             $sender->addTitle("Hey Player this is a test");
+             $item = Item::get(276,0,1);
+             $inv = $sender->getinventory();
+             $item->setCustomName("Testv1's Item");
+             $inv->addItem($item);
+           }elese;
+                $sender->sendMessage("Please Use This Command In Game Please!");
+                return true;
+           }
+    }
+  
